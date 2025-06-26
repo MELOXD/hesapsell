5b4a849e4358b9fc0eae00181b7d23b38c16478a
MELOXD <138215981+MELOXD@users.noreply.github.com>
2025-06-26 13:08:43 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
 // firebase-config.js
+// Firebase 8.x kullandığımız için uygun şekilde export etmiyoruz, global kullanılıyor.
+
 const firebaseConfig = {
   apiKey: "AIzaSyCihxphcfNsfttojsRJ-OcYgGt9zamZZHM",
   authDomain: "lol-hesapsell.firebaseapp.com",
   projectId: "lol-hesapsell",
-  storageBucket: "lol-hesapsell.firebasestorage.app",
+  storageBucket: "lol-hesapsell.appspot.com",
   messagingSenderId: "617230497720",
   appId: "1:617230497720:web:a0315ede821a6d8195814d",
   measurementId: "G-VLWC49S6V2"
