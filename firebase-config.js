4249cd19ab057e209e5cdcf18b253291bfe59587
MELOXD <138215981+MELOXD@users.noreply.github.com>
2025-06-26 12:10:18 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCihxphcfNsfttojsRJ-OcYgGt9zamZZHM",
+  authDomain: "lol-hesapsell.firebaseapp.com",
+  projectId: "lol-hesapsell",
+  storageBucket: "lol-hesapsell.firebasestorage.app",
+  messagingSenderId: "617230497720",
+  appId: "1:617230497720:web:a0315ede821a6d8195814d",
+  measurementId: "G-VLWC49S6V2"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
